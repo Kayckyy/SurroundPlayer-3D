faabebd9c6e9385ccf6a671792f7c6373d761c7c
Kayckyy <ckayck414@gmail.com>
2026-03-29 11:55:26 -0300

    Remove setupReverb from SettingsFragment

--- app/src/main/java/com/sonicsphere/audio/fragments/SettingsFragment.kt
+++ app/src/main/java/com/sonicsphere/audio/fragments/SettingsFragment.kt
@@ -34,7 +34,6 @@ class SettingsFragment : Fragment() {
         setupEqualizer()
         setupBassBoost()
         setupBinaural()
-        setupReverb()
         setupHaas()
         startPeriodicUpdate()
     }
